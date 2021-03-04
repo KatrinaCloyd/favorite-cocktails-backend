3b00d8ef5192358ec88eb7cb71afc69d49bef900
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-03-03 16:10:53 -0800

    backend all set up with six endpoints plus the signin and signup routes, 6 endpoints passing tests

---
+++ data/favorites.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    name: 'MargAHHHHrita',
+    drink_id: 12345,
+    image: 'http://placekitten.com/200/300',
+    mainingredient: 'tequila',
+    owner_id: 1
+  },
+  {
+    name: 'Strong Margarita',
+    drink_id: 12658,
+    image: 'http://placekitten.com/200/300',
+    mainingredient: 'tequila',
+    owner_id: 1
+  },
+  {
+    name: 'Strawberry Margarita',
+    drink_id: 54321,
+    image: 'http://placekitten.com/200/300',
+    mainingredient: 'tequila',
+    owner_id: 1
+  },
+];
